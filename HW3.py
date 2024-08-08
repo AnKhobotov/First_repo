88f0d40faa40bfee4ad21fbc8eb1f1644b791725
AnKhobotov <and7079@gmail.com>
2024-08-09 00:41:18 +0200

    HW3_committed 09_08_2024

--- HW3.py
+++ HW3.py
@@ -1,8 +1,9 @@
 import datetime
 from datetime import datetime
 import random
+import re
 
-#print(datetime.today())
+#First task
 
 def getDaysFromToday(datum):
     try:
@@ -14,33 +15,66 @@ def getDaysFromToday(datum):
         return diff.days
     except ValueError:
         print(f"Datum should be in 'YYYY-MM-DD' format")
-getDaysFromToday("2024.07.30")
+getDaysFromToday("2024.07.31")
 
-def get_numbers_ticket(min, max, quantity):
+#Second task
+
+def get_numbers_ticket(min, max, quantity=int):
     num_set = set()
-    nlist = []
-    q = quantity
-    print(q)
-    #min, max, quantity = int()
-    #a = random.randint(min, max)
-    try:
-        if 0 < min and max < 1000:
-            if min < quantity < max:
-                while  len(num_set) < q :
+    nlist = list(num_set)
+    if min > 0:
+        if 0 < max < 1000:
+            if min < quantity < max:        
+                while int(len(num_set))<quantity:
                     a = random.randint(min, max)
                     num_set.add(a)
-                    #num_list.sort()
-                    quantity -=1
-                    len(num_set)
-                    for i in num_set:
-                        nlist.append(i)
-                        nlist.sort()
-                        #num_list = list[num_list]
-                        #num_list.sort(a)
-                    print(a, nlist, q)
-            print (f"{quantity} should be positive number less then {max}")    
-        print( f"{min} and {max} should be positive number less then 1000")
-    except ValueError:
-        print("Please check! All numbers  should be positive number less then 1000") 
-                    #print(a, nlist, q)
-get_numbers_ticket(1,49,0)     
+                nlist= list(num_set)
+                nlist.sort()
+            return nlist
+        return nlist
+    return nlist
+    
+get_numbers_ticket(1, 49, 6) 
+
+# Third task
+def normalize_phone(phone_number = str):
+    raw_numbers = [
+    "067\\t123 4567",
+    "(095) 234-5678\\n",
+    "+380 44 123 4567",
+    "380501234567",
+    "    +38(050)123-32-34",
+    "     0503451234",
+    "(050)8889900",
+    "38050-111-22-22",
+    "38050 111 22 11   "]
+
+    phone_number = str
+    sanitized_numbers = []
+    for item in raw_numbers:
+        pattern = r"\d"
+        matches = re.findall(pattern, item )
+        item = "".join(matches)
+        item = item[-10:-1]
+        item = "+38" + item
+        sanitized_numbers.append(item)
+        
+    # for phone_number in raw_numbers:
+    #     phone_number = phone_number
+    #     phone_number.strip()
+    #     pattern = r"[\-\(\)\.\s]"
+    #     repl = ""
+    #     phone_number = re.sub(pattern, repl, phone_number)
+    #     phone_number = phone_number[-10:-1]
+    #     phone_number = "+38" + phone_number
+    #     san_num.append(phone_number)
+
+    # for i[0:1] in phone_number:
+    # if phone_number.find("38") is True:
+    #     phone_number = "+".join(phone_number)
+    #     print(phone_number)
+    #     return phone_number
+    # return phone_number
+    print(sanitized_numbers)
+normalize_phone("38050 111 22 11   ")
+
--- experiments.py
+++ experiments.py
@@ -1,26 +1,29 @@
-def funcx10(*args):
-    a = []
-    for i in args:
-        a.append(i * 10)
-    print(a)
-    return a    
-funcx10(1,3,5)
+# def funcx10(*args):
+#     a = []
+#     for i in args:
+#         a.append(i * 10)
+#     print(a)
+#     return a    
+# funcx10(1,3,5)
 
-def funcx2(*args):
-    b = []
-    for i in args:
-        #funcx10(i)
-        #i = i-2
-        b.append(funcx10(i - 2) )
-        print(b)
-    return b
-    print(b)
-funcx2(2,4,6)
+# def funcx2(*args):
+#     b = []
+#     for i in args:
+#         #funcx10(i)
+#         #i = i-2
+#         b.append(funcx10(i - 2) )
+#         print(b)
+#     return b
+#     print(b)
+# funcx2(2,4,6)
 
-a =2
-b= 3
-c = 5
-if (a+b)>=c:
-    print(a)
-else:
-    print(b)
+# a =2
+# b= 3
+# c = 5
+# if (a+b)>=c:
+#     print(a)
+# else:
+#     print(b)
+
+a = set({1,2})
+print(len(a),type(a))
